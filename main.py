c806599198e22e371bd6fe1566ce16aa3bb5179f
davide1729 <arnaldodabrescia90@gmail.com>
2024-12-13 12:48:43 +0100

    minor changes

--- main.py
+++ main.py
@@ -34,3 +34,55 @@ print(df.dtypes)
 
 # print all unique values in the 'Drug_A' and 'Drug_B' columns
 print(df['Drug_A'].unique())
+
+df2 = pd.read_csv('db_drug_interactions.csv')
+
+print(df2.head())
+
+print(df2.shape)
+print(df2.columns)
+print(df2.dtypes)
+
+print(df2['Drug 1'].unique())
+
+# how many unique values are in the 'Drug 1' column?
+print(len(df2['Drug 1'].unique()))
+print(len(df2['Drug 2'].unique()))
+print(len(df2['Interaction Description'].unique()))
+
+
+df3 = 
+
+
+
+
+
+
+
+
+
+
+# we need to perform text analysis on the 'Interaction Description' column
+# to extract the drug interactions
+
+# we will use the nltk library for text analysis
+# nltk: to perform text analysis
+
+import nltk
+from nltk.corpus import stopwords
+
+# stopwords: a list of common words that are not useful for text analysis
+
+nltk.download('stopwords')
+
+# Inside Interactions Description column, we have a lot of text data
+
+# a sample entry for interaction description is:
+print(df2['Interaction Description'][0])
+# this is "Trioxsalen may increase the photosensitizing activities of Verteporfin."
+
+# this means that we need to check that the first drug that is mentioned
+# corresponds to the 'Drug 1' column and the second drug corresponds to the 'Drug 2' column
+
+# we need to extract the drug names from the 'Interaction Description' column
+# and check if they are present in the 'Drug 1' and 'Drug 2' columns
